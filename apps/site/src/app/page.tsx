595b86a5f00dadfe5f4dc41d38c9bdb9bc563f1e
TiagoJSaraiva <saraivajtiago@gmail.com>
2026-06-23 23:04:07 -0300

    Remoção do site que ainda não existe

--- apps/site/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Site Institucional - Template Base</h1>
-      <p>Aplicação Next.js pronta para evolução de conteúdo e SEO.</p>
-    </main>
-  );
-}
